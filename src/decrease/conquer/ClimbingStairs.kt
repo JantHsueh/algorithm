3b2ec5707b4d5f7148a071e4792c85b072671fae
xuexuan <384324069@qq.com>
2020-03-25 18:54:17 +0800

    first commit

---
+++ src/decrease/conquer/ClimbingStairs.kt
@@ -0,0 +1,41 @@
+package decrease.conquer
+
+/**
+ * create by xuexuan
+ * time 2020/3/25 14:18
+ * 70 爬楼梯
+ * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+ * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+ * 注意：给定 n 是一个正整数。
+ */
+
+
+class ClimbingStairs {
+
+
+    fun climbStairs(n: Int): Int {
+
+        var previous = 0
+        var current = 1
+
+        for (x in 1 .. n){
+            val temp = previous + current
+            previous = current
+            current = temp
+        }
+
+        return current
+    }
+
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val cs = ClimbingStairs()
+            val num = 5
+            print("$num 台阶 总共有 ${cs.climbStairs(num)}种方法")
+        }
+    }
+
+
+}
---
+++ src/decrease/conquer/MaximumSubarray.kt
@@ -0,0 +1,79 @@
+package decrease.conquer
+
+/**
+ * create by xuexuan
+ * time 2020/3/25 15:40
+ *
+ * 53. 最大子序和
+ * 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+ *
+ * 也就是邓俊辉 算法训练营，Greatest Slice
+ */
+
+class MaximumSubarray {
+
+
+    /**
+     * 动态规划  时间 O（n） 空间O（1）
+     */
+    fun maxSubArray(nums: IntArray): Int {
+
+
+
+        var ans = nums[0]
+        var currNum = nums[0]
+        for (index in 1 until nums.size) {
+
+            currNum = Math.max(currNum, 0) + nums[index]
+            ans = Math.max(ans,currNum)
+        }
+
+        return ans
+
+    }
+
+
+    /**
+     * 减而治之  时间 O（n） 空间O（1）
+     */
+    fun maxSubArray1(nums: IntArray): Int {
+
+        var gs: Int = nums[0]
+
+        var sum = 0
+        for (index in nums.indices) {
+
+            if ( sum < 0) {
+                //更新
+                sum = 0
+            }
+            sum += nums[index]
+
+            if (sum > gs) {
+                gs = sum
+            }
+        }
+
+        return gs
+    }
+
+
+    /**
+     * 分而治之  O（nlogn）
+     */
+    fun maxSubArray2(nums: IntArray): Int {
+
+        return 0
+    }
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val cs = MaximumSubarray()
+            val intArray = arrayListOf(-2, 1, -3, 4, -1, 2, 1, -5, 4) //的和最大为 6。
+//            val intArray = arrayListOf( 1) //的和最大为 1。
+//            val intArray = arrayListOf(-2, -1, -3) //连续子数组 [4,-1,2,1] 的和最大为 -1。
+            print("$intArray ,最大子序列和 ${cs.maxSubArray(intArray.toIntArray())}")
+        }
+    }
+}
